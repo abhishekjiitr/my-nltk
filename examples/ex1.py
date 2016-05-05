d77fffe4e9b3b1f01d8ed742cf161059872a4886
abhishekjiitr <abhi2254015@gmail.com>
2016-05-05 19:45:49 +0530

    Added initial examples

---
+++ examples/ex1.py
@@ -0,0 +1,6 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+EXAMPLE_TEXT = "Hello Mr. Smith, how are you doing today? The weather is great, and Python is awesome. The sky is pinkish-blue. You shouldn't eat cardboard."
+
+# print(sent_tokenize(EXAMPLE_TEXT))
+print(word_tokenize(EXAMPLE_TEXT))
