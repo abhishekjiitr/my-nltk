4128abcd4f906bb6577d1030608648df8359749f
abhishekjiitr <abhi2254015@gmail.com>
2016-05-06 22:38:08 +0530

    Added 2 more exs

--- examples/ex6.py
+++ examples/ex6.py
@@ -11,16 +11,15 @@ tokenized = custom_sent_tokenizer.tokenize(sample_text)
 
 def process_content():
     try:
-        for i in tokenized:
+        for i in tokenized[5:]:
             words = nltk.word_tokenize(i)
             tagged = nltk.pos_tag(words)
-            chunkGram = r"""Chunk: {<RB.?>*<VB.?>*<NNP>+<NN>?}"""
+
+            chunkGram = r"""Chunk: {<.*>+}
+                                    }<VB.?|IN|DT|TO>+{"""
+
             chunkParser = nltk.RegexpParser(chunkGram)
             chunked = chunkParser.parse(tagged)
-            
-            print(chunked)
-            for subtree in chunked.subtrees(filter=lambda t: t.label() == 'Chunk'):
-                print(subtree)
 
             chunked.draw()
 
